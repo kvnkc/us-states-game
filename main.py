2ef33d471bb58d004bb4a8cde64bd2495d260a8a
Cheung <cheung@tdfcu.com>
2022-10-24 14:20:12 -0400

    added exit function and create a csv with missing states

--- main.py
+++ main.py
@@ -13,25 +13,31 @@ correct_guess = []
 
 while len(correct_guess) < 50:
     answer_state = screen.textinput(
-        title=f'{len(correct_guess)}/50 States correct', prompt="What's another state's name?: ")
-    title_answer = answer_state.title()
+        title=f'{len(correct_guess)}/50 States correct', prompt="What's another state's name?: ").title()
 
     data = pandas.read_csv('50_states.csv')
 
     states = data.state.to_list()
 
-    if title_answer in states:
-        coord = data[data.state == title_answer]
+    # Exits the game and converts missing states into a separate list and csv
+    if answer_state == 'Exit':
+        missing_states = []
+        for state in states:
+            if state not in correct_guess:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv('states_to_learn.csv')
+        break
+    # Checks if title_amswer is one of the states in all the states in the 50_states.csv
+    if answer_state in states:
+        coord = data[data.state == answer_state]
         x_coord = int(coord.x)
         y_coord = int(coord.y)
         turtle.penup()
         turtle.goto(x_coord, y_coord)
-        turtle.write(title_answer)
+        turtle.write(answer_state)
         correct_guess.append(answer_state)
 
         print(coord)
         print(x_coord)
         print(y_coord)
-
-
-screen.exitonclick()
