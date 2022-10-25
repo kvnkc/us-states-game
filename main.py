fef25685db4988207c17027ace6bc128f639a667
kevin cheung <kevin.cheung.kai@gmail.com>
2022-10-24 22:34:15 -0400

    added exit function and able to create a csv with missing state using pandas

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(
-    title='Guess the State', prompt="What's another state's name?: ")
-title_answer = answer_state.title()
+game_is_on = True
 
-data = pandas.read_csv('50_states.csv')
-coord = data[data.state == title_answer]
-x_coord = coord.x
-y_coord = coord.y
-states = data.state.to_list()
+while game_is_on:
+    answer_state = screen.textinput(
+        title='Guess the State', prompt="What's another state's name?: ")
+    title_answer = answer_state.title()
 
-for state in states:
-    if title_answer == state:
-        print(state)
+    data = pandas.read_csv('50_states.csv')
+    coord = data[data.state == title_answer]
+    x_coord = coord.x
+    y_coord = coord.y
+    states = data.state.to_list()
 
-print(coord)
+    for state in states:
+        if title_answer == state:
+            print(state)
+        elif title_answer != state:
+            game_is_on = False
+
+    print(coord)
 
 
 screen.exitonclick()
